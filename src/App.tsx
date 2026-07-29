c249c5838ea3d780cc7a80b37e25ae72c32b2cf6
Jace Whitten <jace.whitten@gmail.com>
2026-07-28 19:52:37 -0700

    Bump download links to v0.2.11

--- src/App.tsx
+++ src/App.tsx
@@ -62,13 +62,13 @@ const blocks = [
 ]
 
 const REPO = 'https://github.com/jacebot/wavesilo'
-const REL = `${REPO}/releases/download/v0.2.10`
+const REL = `${REPO}/releases/download/v0.2.11`
 
 const downloads = [
-  { key: 'mac-arm', os: 'macOS', note: 'Apple Silicon', href: `${REL}/Wave.Silo-0.2.10-arm64.dmg` },
-  { key: 'mac-intel', os: 'macOS', note: 'Intel', href: `${REL}/Wave.Silo-0.2.10.dmg` },
-  { key: 'win', os: 'Windows', note: '.exe installer', href: `${REL}/Wave.Silo.Setup.0.2.10.exe` },
-  { key: 'linux', os: 'Linux', note: '.deb · Debian/Ubuntu', href: `${REL}/Wave.Silo-0.2.10.deb` },
+  { key: 'mac-arm', os: 'macOS', note: 'Apple Silicon', href: `${REL}/Wave.Silo-0.2.11-arm64.dmg` },
+  { key: 'mac-intel', os: 'macOS', note: 'Intel', href: `${REL}/Wave.Silo-0.2.11.dmg` },
+  { key: 'win', os: 'Windows', note: '.exe installer', href: `${REL}/Wave.Silo.Setup.0.2.11.exe` },
+  { key: 'linux', os: 'Linux', note: '.deb · Debian/Ubuntu', href: `${REL}/Wave.Silo-0.2.11.deb` },
 ]
 
 const features = [
@@ -242,7 +242,7 @@ export default function App() {
           </div>
           <p className="sub center">
             Unsigned for now: on macOS right-click &rarr; <em>Open</em>; on Windows choose <em>More info &rarr; Run anyway</em>.
-            {' '}Other Linux distros: <a href={`${REL}/Wave.Silo-0.2.10.tar.gz`}>.tar.gz</a>.
+            {' '}Other Linux distros: <a href={`${REL}/Wave.Silo-0.2.11.tar.gz`}>.tar.gz</a>.
             {' '}<a href={`${REPO}/releases`}>All files &amp; versions &rarr;</a>
           </p>
         </section>
