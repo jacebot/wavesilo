898266bfdbc25cc99d0f1d85c9eb41a7ff836afa
Jace Whitten <jace.whitten@gmail.com>
2026-07-29 22:15:59 -0700

    Bump download links to v0.2.17

--- src/App.tsx
+++ src/App.tsx
@@ -63,15 +63,15 @@ const blocks = [
 ]
 
 const REPO = 'https://github.com/jacebot/wavesilo'
-const REL = `${REPO}/releases/download/v0.2.16`
+const REL = `${REPO}/releases/download/v0.2.17`
 
 const downloads = [
-  { key: 'mac-arm', os: 'macOS', note: 'Apple Silicon', href: `${REL}/Wave.Silo-0.2.16-arm64.dmg` },
-  { key: 'mac-intel', os: 'macOS', note: 'Intel', href: `${REL}/Wave.Silo-0.2.16.dmg` },
-  { key: 'win', os: 'Windows', note: '.exe installer', href: `${REL}/Wave.Silo.Setup.0.2.16.exe` },
-  { key: 'linux', os: 'Linux', note: '.deb · Debian/Ubuntu', href: `${REL}/Wave.Silo-0.2.16.deb` },
+  { key: 'mac-arm', os: 'macOS', note: 'Apple Silicon', href: `${REL}/Wave.Silo-0.2.17-arm64.dmg` },
+  { key: 'mac-intel', os: 'macOS', note: 'Intel', href: `${REL}/Wave.Silo-0.2.17.dmg` },
+  { key: 'win', os: 'Windows', note: '.exe installer', href: `${REL}/Wave.Silo.Setup.0.2.17.exe` },
+  { key: 'linux', os: 'Linux', note: '.deb · Debian/Ubuntu', href: `${REL}/Wave.Silo-0.2.17.deb` },
 ]
-const ARM_DEB = `${REL}/Wave.Silo-0.2.16-arm64.deb`
+const ARM_DEB = `${REL}/Wave.Silo-0.2.17-arm64.deb`
 const VERSION = (REL.match(/v(\d+\.\d+\.\d+)/) ?? [])[1] ?? ''
 
 const features = [
@@ -261,7 +261,7 @@ export default function App() {
           </p>
           <p className="sub center">
             Unsigned for now: on macOS right-click &rarr; <em>Open</em>; on Windows choose <em>More info &rarr; Run anyway</em>.
-            {' '}Other Linux distros: <a href={`${REL}/Wave.Silo-0.2.16.tar.gz`}>.tar.gz</a>.
+            {' '}Other Linux distros: <a href={`${REL}/Wave.Silo-0.2.17.tar.gz`}>.tar.gz</a>.
             {' '}<a href={`${REPO}/releases`}>All files &amp; versions &rarr;</a>
           </p>
         </section>
